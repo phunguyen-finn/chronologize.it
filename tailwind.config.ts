ce0f78750d3abcd3415d5d6164be3ca74b97a923
Phu Nguyen <nnphongphu@gmail.com>
2024-09-02 15:04:17 +0700

    feat: Sfonts and styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ['selector'],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +15,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    // colors: {
+    //   'black': '#0a0a0a',
+    //   'white': '#fafafa',
+    // }
   },
   plugins: [],
 };
