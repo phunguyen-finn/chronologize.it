160eaa17b2675b398e5ed0b24ab1d917024fdcfe
Phu Nguyen <nnphongphu@gmail.com>
2024-09-02 15:04:52 +0700

    feat: wiki media

---
+++ src/services/wiki.service.ts
@@ -0,0 +1,33 @@
+export const WikiService = {
+    gatherRelatedMediaFiles: async (pages: string[]) => {
+        const relatedPages = await Promise.all(pages.map(async (page) => {
+            const response = await fetch(`https://api.wikimedia.org/core/v1/wikipedia/en/search/page?q=${page}&limit=1`);
+            const media = await response.json();
+            if (!media['pages'].length) {
+                return null;
+            }
+            return media['pages'][0];
+        }));
+
+        const mediaFiles = await Promise.all(relatedPages.map(async (page) => {
+            if (!page) {
+                return [];
+            }
+            const response = await fetch(`https://api.wikimedia.org/core/v1/wikipedia/en/page/${page['key']}/links/media`);
+            const media = await response.json();
+            if (!media['files']) {
+                return [];
+            }
+            return media['files'].map((file: any) => {
+                return {
+                    'title': file['title'],
+                    'url': "https://" + file['preferred']['url'].substring(2),
+                    'width': file['preferred']['width'],
+                    'height': file['preferred']['height'],
+                };
+            });
+        }));
+        const flattenedMediaFiles = mediaFiles.flat();
+        return flattenedMediaFiles;
+    }
+}
