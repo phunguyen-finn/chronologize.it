69bb7ba2391200e2437d4fa87ed332d4ff5e4cef
Phu Nguyen <nnphongphu@gmail.com>
2024-09-04 14:52:21 +0700

    fix: escape char: g

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
                 <h1 className="text-4xl md:text-5xl font-bold">About</h1>
                 <div>
 
-                    Get in, we're going back in time! <br /> <br />
+                    Get in, we&quot;re going back in time! <br /> <br />
 
                     <b>chronologize.it</b> is an interactive journey through history
                     that will keep you engaged. Whether you’re fascinated by the
@@ -14,7 +14,7 @@ export default function Page() {
                     lets you explore detailed timelines and immerse yourself in the events that
                     shaped our world. <br /> <br />
 
-                    Born on <i>Vietnam's Independence Day</i> (September 2nd, 2024), this web
+                    Born on <i>Vietnam&quot;s Independence Day</i> (September 2nd, 2024), this web
                     application was inspired by a simple idea: timelines. During my visits to
                     museums for the holiday, I realized that timelines are a fascinating way to uncover the
                     layers of history and understand the world better. Timelines reveal
@@ -31,8 +31,8 @@ export default function Page() {
 
                     <b className="text-lg">Contacts</b>
                     <br />
-                    Hello, I'm Phú—a software engineer who loves to build things. <b>chronologize.it</b> is one
-                    of my creations, and I'm thrilled to share it with you. Thank you for using this platform;
+                    Hello, I&quot;m Phú—a software engineer who loves to build things. <b>chronologize.it</b> is one
+                    of my creations, and I&quot;m thrilled to share it with you. Thank you for using this platform;
                     I hope you enjoy it as much as I do!
 
                     You can contact me via:<br /><br />
@@ -43,7 +43,7 @@ export default function Page() {
                     <b className="text-lg">Development</b>
                     <br />
 
-                    If you're interested in contributing to the development of this site, requesting a feature,
+                    If you&quot;re interested in contributing to the development of this site, requesting a feature,
                     reporting a bug, following development progress, or just want to say hi, feel free to check out the
                     repository <a target="_blank" href="https://github.com/phunguyen-finn/chronologize.it" className="text-sky-500 hover:underline">here.</a>
 
